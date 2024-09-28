c963c2c6b9371aeef04467b390ff5e11a8b81de3
Smexo <larushkinevgeniy.ru@gmail.com>
2024-09-28 11:27:40 +0500

    gorner

--- AlgorithmsLogic/GornerMethod.cs
+++ AlgorithmsLogic/GornerMethod.cs
@@ -2,5 +2,17 @@
 
 public class GornerMethod
 {
-    
+    private int[] Vector { get; init; }
+
+    public GornerMethod(int[] inputVector)
+    {
+        Vector = inputVector;
+    }
+
+    public double Function(double varible, int iterationNumber)
+    {
+        if (iterationNumber >= Vector.Length)
+            return 1;
+        return Vector[iterationNumber] + varible * Function(varible,  iterationNumber + 1);
+    }
 }
