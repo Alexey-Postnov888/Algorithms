63bbe03b559ec180d8cd8f1aa42f5d50da1ed502
Smexo <larushkinevgeniy.ru@gmail.com>
2024-09-28 11:27:24 +0500

    bubble

--- AlgorithmsLogic/BubbleSort.cs
+++ AlgorithmsLogic/BubbleSort.cs
@@ -2,4 +2,27 @@
 
 public class BubbleSort
 {
+    private int[] Vector { get; init; }
+
+    public BubbleSort(int[] inputVector)
+    {
+        Vector = inputVector; 
+    }
+
+    public void Function()
+    {
+        int temp;
+        for (int i = 0; i < Vector.Length; i++)
+        {
+            for(int j = 0; j < Vector.Length - 1; j++)
+            {
+                if (Vector[j + 1] < Vector[j])
+                {
+                    temp = Vector[j + 1];
+                    Vector[j + 1] = Vector[j];
+                    Vector[j] = temp;
+                }
+            }
+        }
+    }
 }
