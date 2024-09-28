200fb847413e56b76a82a2008996c1750e568d00
Smexo <larushkinevgeniy.ru@gmail.com>
2024-09-28 11:27:51 +0500

    multiplication

--- AlgorithmsLogic/MultiplicationOfElements.cs
+++ AlgorithmsLogic/MultiplicationOfElements.cs
@@ -1,6 +1,23 @@
-﻿namespace AlgorithmsLogic;
+﻿using System.Numerics;
+
+namespace AlgorithmsLogic;
 
 public class MultiplicationOfElements
 {
-    
+    private int[] Vector { get; init; }
+
+    public MultiplicationOfElements(int[] inputVector)
+    {
+        Vector = inputVector;
+    }
+
+    public long Function()
+    {
+        long result = 1;
+        for (int i = 0; i < Vector.Length; i++)
+        {
+            result *= Vector[i];
+        }
+        return result;
+    }
 }
