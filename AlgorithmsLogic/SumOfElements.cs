4ef972ddafba7f6d3bd0ad8a4c1bc8f1cd7b14b2
Smexo <larushkinevgeniy.ru@gmail.com>
2024-09-28 11:28:15 +0500

    sum

--- AlgorithmsLogic/SumOfElements.cs
+++ AlgorithmsLogic/SumOfElements.cs
@@ -1,6 +1,23 @@
-﻿namespace AlgorithmsLogic;
+﻿using System.Runtime.CompilerServices;
+
+namespace AlgorithmsLogic;
 
 public class SumOfElements
 {
-    
+    private int[] Vector {  get; init; }
+
+    public SumOfElements(int[] inputVector)
+    {
+        Vector = inputVector;
+    }
+
+    public long Function()
+    {
+        int sum = 0;
+        for (int i = 0; i < Vector.Length; i++)
+        {
+            sum += Vector[i];
+        }
+        return sum;
+    }
 }
