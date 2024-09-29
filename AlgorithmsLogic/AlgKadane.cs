08baecc0462966706ceaeeae22ce6b2d24d5b5c5
Smexo <larushkinevgeniy.ru@gmail.com>
2024-09-29 11:05:34 +0500

    kadane

---
+++ AlgorithmsLogic/AlgKadane.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLogic
+{
+    public class AlgKadane
+    {
+        public static int Function(int[] vector)
+        {
+            int size = vector.Length;
+            int max_so_far = int.MinValue,
+                max_ending_here = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                max_ending_here = max_ending_here + vector[i];
+
+                if (max_so_far < max_ending_here)
+                    max_so_far = max_ending_here;
+
+                if (max_ending_here < 0)
+                    max_ending_here = 0;
+            }
+
+            return max_so_far;
+        }
+    }
+}
