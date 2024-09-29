3cf51caf6241857546bc699dc0c38df6a546dc25
Smexo <larushkinevgeniy.ru@gmail.com>
2024-09-29 10:59:12 +0500

    pow

---
+++ AlgorithmsLogic/PowAlgorithm1.cs
@@ -0,0 +1,23 @@
+﻿namespace AlgorithmsLogic;
+
+public class PowAlgorithm
+{
+    public static void Function(int[] vector, int number)
+    {
+        for(int i = 0; i < vector.Length; i++)
+        {
+            long result = AuxiliaryFunction(number, vector[i]);
+        }
+    }
+
+    private static long AuxiliaryFunction(int number, int degree)
+    {
+        int count = 0;
+        int result = 1;
+        while (count < degree)
+        {
+            result *= number;
+        }
+        return result;
+    }
+}
